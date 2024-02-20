3d82e3b8429ee182a100252aab86fb7cc1bb6856
bivashy <botyrbojey@gmail.com>
2024-02-20 13:46:31 +0600

    Define required attributes for DecimalFields

--- product/models.py
+++ product/models.py
@@ -6,5 +6,5 @@ class Product(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
     preview_image = models.ImageField(upload_to='preview_images/')
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     creation_timestamp = models.DateTimeField(auto_now_add=True)
