0634c9ff1a79d854b52e2dc2abe7ba18e9b25e48
bivashy <botyrbojey@gmail.com>
2024-02-21 01:56:55 +0600

    Register ProductAdmin ModelAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.get_fields()]
