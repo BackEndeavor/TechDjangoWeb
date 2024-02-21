b6b07b64dd3cae6f67a89c525082658ea6f8a2f1
bivashy <botyrbojey@gmail.com>
2024-02-21 19:24:05 +0600

    Add category field

--- product/models.py
+++ product/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=256)
     description = models.TextField()
+    category = models.CharField(max_length=64)
     preview_image = models.ImageField(upload_to='preview_images/')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     creation_timestamp = models.DateTimeField(auto_now_add=True)
