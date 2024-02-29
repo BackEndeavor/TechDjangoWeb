e78072ba574efe9ce3e41631431075cec826411d
bivashy <botyrbojey@gmail.com>
2024-02-29 16:54:08 +0600

    Initialize Vue project in frontend folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,5 @@
+import '../node_modules/flowbite-vue/dist/index.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
